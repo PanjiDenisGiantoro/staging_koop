57abf098e2543fc8e0a718c4fafe43e56d563a72
anwarachilles <hudorianwar07@gmail.com>
2025-12-31 17:22:44 +0700

    list simpanan dalam profile

--- memberEdit.php
+++ memberEdit.php
@@ -1449,11 +1449,230 @@ if (@$tabb == 7) {
 
 // ----------------------------------------- Bahagian simpanan -----------------------------------------
 if (@$tabb == 8) {
-	print '<div class="mb-3">';
-	print '<div class="py-4">';
-	print 'SIMPANAN';
-	print '</div>';
-	print '</div>';
+	// print_r(tosql($pk, "Text"));die;
+$sWhere = '';
+$sWhere = " WHERE a.userID = " . tosql($pk, "Text") . " ";
+$sSQL   = "SELECT
+    a.userID,
+    a.loginID,
+    a.name,
+    a.applyDate,
+    a.groupID,
+    a.email,
+    a.isActive,
+
+    b.memberID,
+    b.memberDate,
+    b.newIC,
+    b.oldIC,
+    b.placeBirth,
+    b.dateBirth,
+    b.staftNo,
+    b.job,
+    b.slipNo,
+    b.dateStarted,
+    b.grossPay,
+    b.netPay,
+    b.address,
+    b.stateID,
+    b.employerName,
+    b.departmentID,
+    b.billingInd,
+    b.officeNo,
+    b.homeNo,
+    b.mobileNo,
+    b.society1,
+    b.societyNo1,
+    b.society2,
+    b.societyNo2,
+    b.society3,
+    b.societyNo3,
+    b.unitShare,
+    b.paymentID,
+    b.bankName,
+    b.bankBranch,
+    b.bankAccount,
+    b.status AS userdetails_status,
+    b.approvedDate,
+    b.rejectedDate,
+    b.updatedDate,
+    b.updatedBy,
+    b.remark,
+
+    c.id AS depositorID,
+    c.accountNumber,
+    c.Code_simpanan,
+    c.balance,
+    c.nominal_simpanan,
+    c.sumber_dana,
+    c.status AS depositor_status,
+    c.dateApply,
+    c.updateUserID,
+    c.dateUpdateUserID,
+
+    g.name AS namaSimpanan
+
+FROM users a
+JOIN userdetails b
+    ON a.userID COLLATE latin1_general_ci = b.userID COLLATE latin1_general_ci
+JOIN depositoracc c
+    ON c.UserID COLLATE latin1_general_ci = a.userID COLLATE latin1_general_ci
+LEFT JOIN general g
+    ON g.ID = c.Code_simpanan";
+
+$sSQL = $sSQL . $sWhere . " ORDER BY c.dateApply DESC";
+$GetDepositAcc = &$conn->Execute($sSQL);
+
+?>
+<div class="card">
+  <div class="card-body">
+
+    <form name="MyForm" action="?vw=aktivitiLog&amp;mn=901" method="post">
+      <input type="hidden" name="action">
+      <input type="hidden" name="pk" value="">
+      <input type="hidden" name="filter" value="0">
+      <div class="table-responsive">
+        <h5 class="card-title">SIMPANAN &nbsp;</h5>
+        <table border="0" cellspacing="1" cellpadding="3" width="100%" align="center">
+          <tbody>
+            <tr valign="top">
+              <td valign="top">
+                <table border="0" cellspacing="1" cellpadding="2" width="100%" class="table table-sm table-striped">
+									<tbody>
+										<tr class="table-primary">
+											<td nowrap="" align="center">NO</td>
+											<td nowrap="">Nama Simpanan</td>
+											<td nowrap="" align="center">No Rekening</td>
+											<td nowrap="" align="center">Saldo</td>
+											<td nowrap="" align="center">Status</td>
+										</tr>
+										<?php 
+											while (!$GetDepositAcc->EOF) {
+
+												$status = '';
+												if ($GetDepositAcc->fields(depositor_status) == 0) {
+													$status = '<b class="text-secondary">Dalam Proses</b>';
+												}else if ($GetDepositAcc->fields(depositor_status) == 1) {
+													$status = '<b class="text-primary">Disetujui</b>';
+												}else if ($GetDepositAcc->fields(depositor_status) == 2) {
+													$status = '<b class="text-danger">Ditolak</b>';
+												}
+
+												$balance = number_format($GetDepositAcc->fields(balance), 2);
+										?>
+										<tr class="table-light">
+											<td class="Data" align="center">1</td>
+											<td class="Data"><?php echo $GetDepositAcc->fields(namaSimpanan); ?></td>
+											<td class="Data"><?php echo $GetDepositAcc->fields(accountNumber); ?></td>
+											<td class="Data">Rp <?php echo $balance ?></td>
+											<td class="Data"><?php echo $status; ?></td>
+										</tr>
+										<?php
+												$GetDepositAcc->MoveNext();
+											} ?>
+									</tbody>
+                </table>
+              </td>
+            </tr>
+            <tr>
+              <td class="textFont">Jumlah Data : <b>850</b></td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    </form>
+
+
+    <script language="JavaScript">
+      var allChecked = false;
+      function ITRViewSelectAll() {
+        e = document.MyForm.elements;
+        allChecked = !allChecked;
+        for (c = 0; c < e.length; c++) {
+          if (e[c].type == "checkbox" && e[c].name != "all") {
+            e[c].checked = allChecked;
+          }
+        }
+      }
+
+      function ITRActionButtonClick(v) {
+        e = document.MyForm;
+        if (e == null) {
+          alert('Sila pastikan nama form diwujudkan.!');
+        } else {
+          count = 0;
+          for (c = 0; c < e.elements.length; c++) {
+            if (e.elements[c].name == "pk[]" && e.elements[c].checked) {
+              count++;
+            }
+          }
+
+          if (count == 0) {
+            alert('Sila pilih rekod yang hendak di' + v + 'kan.');
+          } else {
+            if (confirm(count + ' rekod hendak di' + v + 'kan?')) {
+              e.action.value = v;
+              e.submit();
+            }
+          }
+        }
+      }
+
+      function ITRActionButtonClickStatus(v) {
+        var strStatus = "";
+        e = document.MyForm;
+        if (e == null) {
+          alert('Sila pastikan nama form diwujudkan.!');
+        } else {
+          count = 0;
+          j = 0;
+          for (c = 0; c < e.elements.length; c++) {
+            if (e.elements[c].name == "pk[]" && e.elements[c].checked) {
+              pk = e.elements[c].value;
+              strStatus = strStatus + ":" + pk;
+              count++;
+            }
+          }
+
+          if (count == 0) {
+            alert('Sila pilih rekod yang hendak di' + v + 'kan.');
+          } else {
+            if (confirm(count + ' rekod hendak di' + v + 'kan?')) {
+              //e.submit();
+              window.location.href = "memberStatus.php?pk=" + strStatus;
+            }
+          }
+        }
+      }
+
+      function ITRActionButtonStatus() {
+        e = document.MyForm;
+        if (e == null) {
+          alert('Sila pastikan nama form diwujudkan.!');
+        } else {
+          count = 0;
+          for (c = 0; c < e.elements.length; c++) {
+            if (e.elements[c].name == "pk[]" && e.elements[c].checked) {
+              count++;
+              pk = e.elements[c].value;
+            }
+          }
+
+          if (count != 1) {
+            alert('Sila pilih satu rekod sahaja untuk kemaskini status');
+          } else {
+            window.location.href = "memberStatus.php?pk=" + pk;
+          }
+        }
+      }
+      function doListAll() {
+        c = document.forms['MyForm'].pg;
+        document.location = "?vw=aktivitiLog&mn=901?&StartRec=1&pg=" + c.options[c.selectedIndex].value;
+      }
+    </script>
+  </div>
+</div>
+<?php
 }
 
 // ----------------------------------------- Bahagian pembiayaan -----------------------------------------
@@ -1472,7 +1691,7 @@ if (@$tabb == 10) {
 
 // $sWhere = " ID is not null and (byID > 0 or byID like 'a%') and report <> ''";
 // $sWhere = " WHERE `byID` = " . tosql($pk, "Text") . " ";
-$sWhere = "";
+$sWhere = " WHERE byID = " . tosql($pk, "Text") . " ";
 $sSQL   = "SELECT * FROM `activitylog`";
 $sSQL   = $sSQL . $sWhere . " ORDER BY `activityDate` DESC";
 $GetMember = &$conn->Execute($sSQL);
